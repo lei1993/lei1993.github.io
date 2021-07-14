67ba5bce521f91986ab807847730d269724e0d71
leizhao <1052356414@qq.com>
2021-07-14 16:48:20 +0800

    test github action

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -5,5 +5,5 @@ git init
 git config  user.name "lei1993"
 git config  user.email "1052356414@qq.com"
 git add -A
-git commit -m '部署'
+git commit -m 'deploy'
 git push --force "https://${TOKEN}github.com/lei1993/lei1993.github.io.git" "master:master"
