f9e3b2cc6d4bb2b1d5548325960140e9329ff2c4
leizhao <1052356414@qq.com>
2021-07-14 16:18:24 +0800

    test github action

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -1,8 +1,9 @@
 # Publish to GitHub Pages
 # shellcheck disable=SC2164
-cd .vuepress/dist
+cd docs/.vuepress/dist
 git init
 git config --global user.name "lei1993"
+git config --global user.email "1052356414@qq.com"
 git add -A
 git commit -m '部署'
 git push --force "https://${TOKEN}@github.com/lei1993/lei1993.github.io.git" "master:master"
