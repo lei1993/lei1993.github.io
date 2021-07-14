0eadd4bc97ec437e41d977e11e4b2cc2748839ca
leizhao <1052356414@qq.com>
2021-07-14 14:34:06 +0800

    test github action

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -9,8 +9,8 @@ vuepress build
 # Publish to GitHub Pages
 cd .vuepress/dist
 git init
-git config user.name "lei1993"
-git config user.email "1052356414@qq.com"
+git config --global user.name "lei1993"
+git config --global user.email "1052356414@qq.com"
 git add -A
-git commit -m "[vuepress] update docs"
-git push --force "https://${TOKEN}@github.com/lei1993/lei1993.github.io.git" "master:master"
+git commit -m 'deploy'
+git push --quiet --force "https://${TOKEN}@github.com/lei1993/lei1993.github.io.git" "master:master"
