42067a473ceab17d0e0e2153f8abc7e8eaad974d
leizhao <1052356414@qq.com>
2021-07-14 15:09:45 +0800

    test github action

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -13,4 +13,4 @@ git config --global user.name "lei1993"
 git config --global user.email "1052356414@qq.com"
 git add -A
 git commit -m 'deploy'
-git push --quiet --force "https://github.com/lei1993/lei1993.github.io.git" "master:master"
+git push -f git@github.com:lei1993/lei1993.github.io.git master
